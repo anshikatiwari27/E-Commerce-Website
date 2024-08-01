1f3e9713333bcbb258101899e3f7cf0ea07ff3f7
Anshika Tiwari <112117600+anshikatiwari27@users.noreply.github.com>
2024-08-02 00:00:09 +0530

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,13 +1,37 @@
-import React from 'react'
-import Navbar from './Components/Navbar/Navbar'
-import Admin from './Pages/Admin/Admin'
-const App = () => {
+import './App.css';
+import Navbar from './Components/Navbar/Navbar';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ShopCategory from './pages/ShopCategory';
+import Product from './pages/Product';
+import LoginSignup from './pages/LoginSignup';
+import Shop from './pages/Shop';
+import Cart from './pages/Cart';
+import Footer from './Components/Footer/Footer';
+import men_banner from './Components/Asset/banner_mens.png';
+import women_banner from './Components/Asset/banner_women.png'
+import kid_banner from './Components/Asset/banner_kids.png';
+
+function App() {
   return (
     <div>
-     <Navbar></Navbar> 
-     <Admin></Admin>
+      <BrowserRouter>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Shop/>}/>
+        <Route path='/mens' element={<ShopCategory banner={men_banner} category="men"/>}/>
+        <Route path='/womens' element={<ShopCategory banner={women_banner} category="women"/>}/>
+        <Route path='/kids' element={<ShopCategory banner={kid_banner} category="kid"/>}/>
+        <Route path='product' element={<Product/>}>
+
+        <Route path=':productId' element={<Product/>}/>
+        </Route>
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='/login' element={<LoginSignup/>}/> 
+      </Routes>
+      <Footer/>
+      </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
